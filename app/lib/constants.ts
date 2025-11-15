40523c61af2073d9c21d8b248681c7941d54647c
rodrigogborgia <rodrigo@gamificagroup.com>
2025-11-15 08:14:04 -0300

    Dexter_Casual_Front_public

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,6 +1,6 @@
 export const AVATARS = [
   {
-    avatar_id: "Dexter_Lawyer_Sitting_public", 
+    avatar_id: "Dexter_Casual_Front_public", 
     name: "Sammy el Sommelier",
     voice_id: "7a544b76e07648849ed54617f18ea280",
   },
